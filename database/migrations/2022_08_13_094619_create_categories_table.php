49057440906e0a3d2200231d0659abeb83a39a5a
AunggKyawThu <aung@easysheets.net>
2024-02-15 23:51:50 +0630

    ADD: last commit

--- database/migrations/2022_08_13_094619_create_categories_table.php
+++ database/migrations/2022_08_13_094619_create_categories_table.php
@@ -23,6 +23,22 @@ return new class extends Migration
             $table->integer('type_id');
             $table->timestamps();
         });
+        $categoryOfArticles = ['Poems', 'Articles', 'Essays', 'Break Free', 'Student Voice', 'Short Novel'];
+        $categoryOfNews = ['Student News', 'People Message', 'War News', 'Weekly News', 'Brutal News of Military', 'Politic News', 'CDM News', 'Nway Oo Diary'];
+        foreach ($categoryOfArticles as $category) {
+            Category::create([
+                'name' => $category,
+                'slug' => Str::slug($category),
+                'type_id' => PostType::Article,
+            ]);
+        }
+        foreach ($categoryOfNews as $category) {
+            Category::create([
+                'name' => $category,
+                'slug' => Str::slug($category),
+                'type_id' => PostType::News,
+            ]);
+        }
     }
 
     /**
